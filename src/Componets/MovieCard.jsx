d7c1d6ad894f41d643a2b2329ec06cf5bf2332a5
Felipe ASP <felipe.asp2@hotmail.com>
2024-03-27 03:56:41 -0300

    Corrige títulos MovieCard

--- src/Componets/MovieCard.jsx
+++ src/Componets/MovieCard.jsx
@@ -10,6 +10,10 @@ function MovieCard({ movie }) {
         hover:border-[3px] border-gray-400 cursor-pointer
         hover:scale-110 transition-all duration-150 ease-in"
       />
+      <h2
+        className="w-[110px] md:w-[260px] text-white
+    mt-2"
+      ></h2>
     </>
   );
 }
